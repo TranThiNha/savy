03b4f272818701aa2314b88d300cfceb0f85f702
Nha Tran Thi <nhatranthi@Nhas-MacBook-Air.local>
2022-08-12 23:51:58 +0700

    update UI sider

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,6 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
   constructor() {}
+
+  MENU_SIDER = [{
+      name: 'Trang chủ',
+      icon: "home.svg"
+    },
+    {
+      name: 'Danh sách KH Savy',
+      icon: "people.svg"
+    },
+    {
+      name: 'Quản lý tài khoản',
+      icon: "wallet.svg",
+      sections: [{
+          name: 'Khoá/Mở khoá tài khoản',
+        },
+        {
+          name: 'Cấp lại mật khẩu',
+        },
+        {
+          name: 'Danh sách đã được phê duyệt',
+        },
+      ],
+    },
+    {
+      name: 'Quản lý thông báo',
+      icon: "noti.svg"
+    },
+    {
+      name: 'Phê duyệt tập trung',
+      icon: "check.svg"
+    },
+  ];
 
   ngOnInit(): void {}
 }
